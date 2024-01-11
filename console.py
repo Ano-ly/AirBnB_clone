c22ad054d29cb22fefd5c6d298b6bbe413b6065a
Ano-ly <amarachiuvere@student.oauife.edu.ng>
2024-01-11 05:56:04 +0100

    Add python file for command line interpreter

---
+++ console.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+import cmd
+
+
+class HBNBCommand(cmd.Cmd):
+    """
+    The following class creates a command line interface
+    for the AirBnB project
+    """
+
+    prompt = "(hbnb) "
+
+    def emptyline(self):
+        """Overwrites the emptyline function."""
+        pass
+
+    def do_EOF(self, line):
+        """Exits the program"""
+        return True
+
+    def do_quit(self, line):
+        """Quit command to exit the program\n"""
+        return True
+
+
+if __name__ == "__main__":
+    HBNBCommand().cmdloop()
