41d9dc4a0d7a7e75f41ecc72861062585190766f
AxiomDays <tobeoke@gmail.com>
2024-01-13 22:43:14 +0100

    task0

--- models/base_model.py
+++ models/base_model.py
@@ -8,12 +8,21 @@ from datetime import datetime, date
 class BaseModel:
     """This is the BaseModel class"""
 
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """Initialises instance"""
 
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+        if kwargs is not None:
+            for key in kwargs:
+                if key == "created_at":
+                    self.__dict__["created_at"] = datetime.isoformat(kwargs["created_at"])
+                elif key == "updated_at":
+                    self.__dict__["updated_at"] = datetime.isoformat(kwargs["updated_at"])
+                else:
+                    self.__dict__[key] = kwargs[key]
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
     def __str__(self):
         """Overwrites __str__ method"""
