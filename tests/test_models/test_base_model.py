ce8aada12cf5ec8351187f29f24302d2c598db2c
Ano-ly <amarachiuvere@student.oauife.edu.ng>
2024-01-14 17:50:42 +0100

    Made some changes

---
+++ tests/test_models/test_base_model.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""This is a base model testing file"""
+
+
+import unittest
+from datetime import datetime, date
+
+
+from models.base_model import BaseModel
+
+
+class Test_Base_Model(unittest.TestCase):
+    """This tests the methods in the class BaseModel"""
+
+    def setUp(self):
+        """The initial setup function"""
+
+        self.Base1 = BaseModel()
+        self.assertEqual(self.Base1.updated_at, self.Base1.created_at)
+
+    #def test_init_(self):
+        """Tests the initialisation of a basemodel object"""
+
+
+    def test_save(self):
+        """Test the save method of the class BaseModel"""
+
+        x = self.Base1.updated_at
+        self.Base1.save()
+        y = self.Base1.updated_at
+        self.assertFalse(x == y)
+
+
+if __name__ == "__main__":
+    unittest.main()
